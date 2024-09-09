d17f8bfa43cb6479412cb446c238ac543685a31a
jesus is king <fadi.b.alhabib@gmail.com>
2024-09-09 13:07:16 +0300

    fixed filtering

--- app/Http/Filters/V1/StoryFilter.php
+++ app/Http/Filters/V1/StoryFilter.php
@@ -2,7 +2,8 @@
 
 namespace App\Http\Filters\V1;
 
-class StoryFilter extends QueryFilter{
+class StoryFilter extends QueryFilter
+{
     protected $sortable = [
         'clicks',
         'teller',
@@ -11,45 +12,52 @@ class StoryFilter extends QueryFilter{
     ];
 
 
-    public function clicks($value){
+    public function clicks($value)
+    {
         $values = explode(',', $value);
-        if(count($values) > 1){
+        if (count($values) > 1) {
             return $this->builder->whereBetween('clicks', $values);
         }
         return $this->builder->where('clicks', $values);
     }
 
-    public function state($value){
+    public function state($value)
+    {
         return $this->builder->where('state_id', $value);
     }
 
-    public function title($value){
-        $likeStr = str_replace('*', '%', $value);
-        return $this->builder->where('title', 'like', $likeStr);
+    public function title($value)
+    {
+        // $likeStr = str_replace('*', '%', $value);
+        return $this->builder->where('title', 'like', "%" . $value . "%");
     }
 
-    public function body($value){
-        $likeStr = str_replace('*', '%', $value);
-        return $this->builder->where('body', 'like', $likeStr);
+    public function body($value)
+    {
+        // $likeStr = str_replace('*', '%', $value);
+        return $this->builder->where('body', 'like', "%" . $value . "%");
     }
 
-    public function teller($value){
-        $likeStr = str_replace('*', '%', $value);
-        return $this->builder->where('teller', 'like', $likeStr);
+    public function teller($value)
+    {
+        // $likeStr = str_replace('*', '%', $value);
+        return $this->builder->where('teller', 'like', "%" . $value . "%");
     }
 
-    public function createdAt($value){
+    public function createdAt($value)
+    {
         $dates = explode(',', $value);
 
-        if(count($dates)){
+        if (count($dates)) {
             return $this->builder->whereBetween('created_at', $dates);
         }
 
         return $this->builder->where('created_at', $dates);
     }
 
-    public function tag($value){
-        return $this->builder->whereHas('tags', function($query) use ($value) {
+    public function tag($value)
+    {
+        return $this->builder->whereHas('tags', function ($query) use ($value) {
             $query->where('tags.id', $value);
         });
     }
